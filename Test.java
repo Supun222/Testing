2a33c62155e3f20c7884878d10dd6c13a6103ca3
LasithEranga <73494729+LasithEranga@users.noreply.github.com>
2020-11-14 22:12:12 +0530

    Test.java added.

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+	
+	public static void main(String args[]){
+	
+		System.out.println(Supun.print("Supun"));
+		System.out.println(Dilumi.print("Dilumi"));
+		System.out.println(PK.print("PK"));
+	
+	}
+
+
+}
