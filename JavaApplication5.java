b492e22c5fbd128c949931c302a6249c31f024a0
Supun222 <74459467+Supun222@users.noreply.github.com>
2020-11-14 22:00:18 +0530

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,17 @@
+
+package javaapplication5;
+
+/**
+ *
+ * @author Gunasekara
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+    }
+    
+}
