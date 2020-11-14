d396283f59581d7f32fb143cdf5dabd2c4ae4a1f
LasithEranga <73494729+LasithEranga@users.noreply.github.com>
2020-11-14 22:28:05 +0530

    Updated

--- Test.java
+++ Test.java
@@ -1,11 +1,12 @@
 class Test{
 	
 	public static void main(String args[]){
-	
+	public static void main(String args[]){
 		System.out.println(Supun.print("Supun"));
 		System.out.println(Dilumi.print("Dilumi"));
 		System.out.println(PK.print("PK"));
 	
+	}
 	}
 
 
